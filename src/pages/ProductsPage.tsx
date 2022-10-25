fcc15c1f0141d8b83155c8010c3bdaab2eb713b4
meryem.sekkat <meryem.sekkat@alithya.com>
2022-10-25 09:45:35 +0100

    add new pages

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,13 @@
+
+import React, { useEffect } from 'react'
+
+const ProductsPage = () => {
+   return (
+    <main>
+      <div>i m product</div>
+    </main>
+  )
+}
+
+
+export default ProductsPage
