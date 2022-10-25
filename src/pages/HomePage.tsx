3810544c04ac989f5b391991104a6823d7508325
meryem.sekkat <meryem.sekkat@alithya.com>
2022-10-25 16:01:45 +0100

    add product component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,10 @@
-import NavBar from "../components/NavBar";
-
-
 import React  from 'react'
 
 const HomePage = () => {
   return (
     <main>
-      <NavBar/>
        <div className="container mx-auto ">
+
       </div>
     </main>
   )
